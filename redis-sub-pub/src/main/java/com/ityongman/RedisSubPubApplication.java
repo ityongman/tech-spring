cd5cd6b87e9eb0c024d7b3abc7f8c896f232b1f3
senge <senga.she@dingtone.me>
2019-09-27 22:10:23 +0800

    创建新项目redis-sub-pub

---
+++ redis-sub-pub/src/main/java/com/ityongman/RedisSubPubApplication.java
@@ -0,0 +1,13 @@
+package com.ityongman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSubPubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSubPubApplication.class, args);
+    }
+
+}
---
+++ spring-aop-print-log/readMe.md
@@ -0,0 +1,3 @@
+## Spring boot 打印controller层请求参数信息
+
+### 一、
