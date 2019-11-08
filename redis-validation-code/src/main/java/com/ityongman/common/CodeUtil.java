4a3a617a7b72adc2b600c72ee2075c63d985f2fb
senge <senga.she@dingtone.me>
2019-11-08 09:24:49 +0800

    去掉无用注释

--- redis-validation-code/src/main/java/com/ityongman/common/CodeUtil.java
+++ redis-validation-code/src/main/java/com/ityongman/common/CodeUtil.java
@@ -6,8 +6,8 @@ public class CodeUtil {
 
     /**
      * 自定义进制字符集
-     * <li>32个数: 8个数字(去除'0'和'1'), 24个小写字母(去除 'o'和'l')</li>
-     * <li>NOTE: 数字'0'和'1'容易与小写字母'o'和'l'混淆</li>
+     * 32个数: 8个数字(去除'0'和'1'), 24个小写字母(去除 'o'和'l')
+     * NOTE: 数字'0'和'1'容易与小写字母'o'和'l'混淆
      */
     private static final char[] RADIX_CHAR_ARRAY_LOWERCASE = new char[] {
             'p', 'i', 'm', '8',
@@ -24,7 +24,7 @@ public class CodeUtil {
 
     /**
      * 唯一码的最小长度
-     * <li>若 id值 较小, 映射得到的自定义进制字符串, 长度小于该值, 则填充随机字符串</li>
+     * 若 id值 较小, 映射得到的自定义进制字符串, 长度小于该值, 则填充随机字符串
      */
     private static final int CODE_MIN_LENGTH = 6; // 需要可以扩展, 这里暂时不做
 
